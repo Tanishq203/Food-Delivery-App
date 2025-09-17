62331372544c0d6e0545fa8b9dbfdca2c558b3ba
Tanishq Thakre <129864649+Tanishq203@users.noreply.github.com>
2025-09-17 22:11:25 +0530

    Update Apicalling.jsx

--- src/components/Apicalling.jsx
+++ src/components/Apicalling.jsx
@@ -5,7 +5,7 @@ function Apicalling(){
     const [allRestaurant, setAllRestaurants] = useState([]);
 
     useEffect(()=>{        
-        const API = "/api/dapi/restaurants/list/v5?lat=18.52110&lng=73.85020&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        const API = "/api/restaurants/list/v5?lat=18.52110&lng=73.85020&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
         async function calling(){
             const resp = await axios.get(API);
             // console.log(resp.data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
