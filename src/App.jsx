552a3df7ade446da2883c00fc77ae1cab17e1a93
Tanishq Thakre <129864649+Tanishq203@users.noreply.github.com>
2025-09-17 21:28:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Apicalling from "./components/Apicalling";
-import Body from "./components/body";
+import Body from "./components/Body";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import appStore from "./utils/appStore";
